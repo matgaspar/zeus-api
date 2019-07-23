8db44aca8bb59c7c1207cff835afd2431bff4191
matgaspar <matheusgaspar13@hotmail.com>
2019-07-22 21:33:02 -0300

    Updated: deploy Node.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const CONFIG = {
   app: process.env.APP || 'production',
-  port: process.env.PORT || '3000',
+  port: process.env.PORT || '7777',
   DB: {
     timezone: process.env.DB_TIMEZONE || 'Etc/GMT-3' || 'America/Sao_Paulo',
     dialect: process.env.DB_DIALECT || 'mariadb',
